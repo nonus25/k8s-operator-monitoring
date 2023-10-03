6811ef3eac6eb2f1b0158a1f0dc92e18cd6b6c43
Augustyn Chmiel <auc@softwareag.com>
2023-10-03 09:21:30 +0200

    clueanup

--- test/tools/tool.go
+++
@@ -1,9 +0,0 @@
-package tools
-
-import "os"
-
-func GetMyNamespace() string {
-	namespace, _ := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
-
-	return string(namespace)
-}
